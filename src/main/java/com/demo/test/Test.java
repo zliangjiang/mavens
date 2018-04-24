9c2ed84e8707e2e8f43cdc42037c3bb35c8b0869
BlackDragon <309859094@qq.com>
2018-04-24 21:39:44 +0800

    创建git项目

---
+++ src/main/java/com/demo/test/Test.java
@@ -0,0 +1,15 @@
+package com.demo.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(5,8);
+	}
+
+	public static int add(int a, int b) {
+		int c=a+b;
+		int d=c+9;
+		return d;
+	}
+}
