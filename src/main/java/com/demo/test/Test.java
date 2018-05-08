11774470d9d6900511e018334c246115136a37dc
Administrator <Administrator@DESKTOP-1BGD0FN.mshome.net>
2018-05-08 21:35:35 +0800

    20180508

--- src/main/java/com/demo/test/Test.java
+++ src/main/java/com/demo/test/Test.java
@@ -4,12 +4,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		add(5,8);
+		//add(5,8);
+		//System.out.println(1/0);
+		System.out.println(System.getProperty("file.encoding"));
+		String text = "大家好";
+		System.out.println(text); 
+		
 	}
 
 	public static int add(int a, int b) {
 		int c=a+b;
 		int d=c+9;
 		return d;
+		
 	}
 }
