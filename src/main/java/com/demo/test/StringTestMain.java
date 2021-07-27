817187e73068db592ca56f3ba12682ee5ede0bdc
Administrator <Administrator@192.168.253.1>
2021-07-27 20:21:57 +0800

    20210727添加StringTestMain测试类

--- src/main/java/com/demo/test/StringTestMain.java
+++ src/main/java/com/demo/test/StringTestMain.java
@@ -5,7 +5,7 @@ public class StringTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "Hello World !";
-		int i_arr[] = str.toString().codePoints().toArray();
+		//int i_arr[] = str.toString().codePoints().toArray();
 		for(int i = 0; i< str.length(); i++)	{
 			System.out.println(i);
 			System.out.println(str.toString().codePointAt(i));
